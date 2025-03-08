766d976c37a5f06c93b949450f791e5c8ced6b02
sunil9958 <72134489+sunil9958@users.noreply.github.com>
2025-03-08 13:55:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ HOST = "localhost"  # redis host uri
 PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002386000747  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
 COOKIE = ""
-ADMINS = [1317173146]
+ADMINS = [5390137933]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "Teraboxassdddbot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = "@PvFilmsX"
 
 PUBLIC_EARN_API = ""  # https://publicearn.com/api
